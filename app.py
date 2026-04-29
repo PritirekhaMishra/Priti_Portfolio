ac9edbbbe821861da3bdac0dd6ccebb6a812ece0
Pritirekha Mishra <pritirekha7978@gmail.com>
2026-04-29 16:51:54 +0530

    Use Brevo SMTP port 2525

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ CORS(app)
 
 
 app.config["MAIL_SERVER"] = "smtp-relay.brevo.com"
-app.config["MAIL_PORT"] = 587
+app.config["MAIL_PORT"] = 2525
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
+
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
 app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("EMAIL_USER")
+app.config["MAIL_DEFAULT_SENDER"] = "pritirekha7978@gmail.com"
 
 mail = Mail(app)
 
