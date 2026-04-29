4781e013b66ecba35ce904b13fa29eb77644b195
Pritirekha Mishra <pritirekha7978@gmail.com>
2026-04-29 13:42:09 +0530

    Use Gmail SSL SMTP

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ app.secret_key = os.getenv("SECRET_KEY")
 CORS(app)
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
 app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("EMAIL_USER")
